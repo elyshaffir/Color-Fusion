ef10d4aaa291177f6e4fe662b330b5ac8741ec1e
DanGilboa <dan.gilboa2403@gmail.com>
2023-05-27 01:18:52 +0300

    added platform and spawner

---
+++ Assets/PlatformSpawner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _platformPrefab;
+
+    private void Update()
+    {
+        
+    }
+}
