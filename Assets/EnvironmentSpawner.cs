f4782ddc22076289f479dfe053a16d1567fe977c
Ely Shaffir <elyshaffir@gmail.com>
2023-05-27 17:48:09 +0300

    EnvironmentSpawner more parameterised

--- Assets/EnvironmentSpawner.cs
+++ Assets/EnvironmentSpawner.cs
@@ -5,9 +5,8 @@ public class EnvironmentSpawner : MonoBehaviour
 {
     public GameObject environmentPrefab;
 
-    public int width = 256; // todo this can be increased a lot
-    public int height = 256;
-    public float textureScale = 1f;
+    public Vector2Int resolutionMultiplier = new Vector2Int(20, 20);
+    public float noiseScale = 1f;
 
     private List<GameObject> environments = new List<GameObject>();
 
@@ -15,6 +14,7 @@ public class EnvironmentSpawner : MonoBehaviour
     private float nextYSpawn;
     private float _spawnYInterval;
     private Vector2 _environmentScale;
+    private Vector2Int _environmentResolution;
 
     public bool TryGetEnvironmentByLocation(Vector3 location, out ColorsEnvironment outEnvironment)
     {
@@ -38,6 +38,7 @@ public class EnvironmentSpawner : MonoBehaviour
         _spawnYInterval = environmentPrefab.transform.localScale.y;
         _environmentScale = 2 *
             Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        _environmentResolution = new Vector2Int((int)_environmentScale.x, (int)_environmentScale.y) * resolutionMultiplier;
     }
 
     void FixedUpdate()
@@ -68,24 +69,24 @@ public class EnvironmentSpawner : MonoBehaviour
         Texture2D generatedTexture = GenerateTexture();
         newEnvironment.GetComponent<ColorsEnvironment>().texture = generatedTexture;
         newEnvironment.GetComponent<Renderer>().material.mainTexture = generatedTexture;
-        newEnvironment.GetComponent<ColorsEnvironment>().width = width;
-        newEnvironment.GetComponent<ColorsEnvironment>().height = height;
+        newEnvironment.GetComponent<ColorsEnvironment>().width = _environmentResolution.x;
+        newEnvironment.GetComponent<ColorsEnvironment>().height = _environmentResolution.y;
 
         newEnvironment.transform.localScale =
             new Vector3(_environmentScale.x, _environmentScale.y, newEnvironment.transform.localScale.z);
-        Debug.Log(newEnvironment.transform.localScale);
         environments.Add(newEnvironment);
     }
 
     private Texture2D GenerateTexture()
     {
-        Texture2D texture = new Texture2D(width, height);
-        for (int x = 0; x < width; x++)
+        Debug.Log(_environmentResolution);
+        Texture2D texture = new Texture2D(_environmentResolution.x, _environmentResolution.y);
+        for (int x = 0; x < _environmentResolution.x; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < _environmentResolution.y; y++)
             {
-                float xCoord = (float)x / width * textureScale;
-                float yCoord = (float)y / height * textureScale + (nextYSpawn / 10.0f); // todo hard coded
+                float xCoord = (float)x / _environmentResolution.x * noiseScale;
+                float yCoord = (float)y / _environmentResolution.y * noiseScale + (nextYSpawn / 10.0f); // todo hard coded
                 float sample = Mathf.PerlinNoise(xCoord + _seed, yCoord + _seed);
                 Color color;
                 if (sample < 0.3f)
