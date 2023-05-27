abc50c25cd475eef3ca0917c0d745e9ce9e568b2
DanGilboa <dan.gilboa2403@gmail.com>
2023-05-27 14:38:43 +0300

    platform spawner works

--- Assets/CameraChase.cs
+++ Assets/CameraChase.cs
@@ -4,6 +4,10 @@ public class CameraChase : MonoBehaviour
 {
     [SerializeField] private Transform _mainGuy;
 
+   // public float maxSpeed = 0.02f;
+
+    private float _currentSpeed = 0.015f;
+
     private void LateUpdate()
     {
         if (_mainGuy.position.y > transform.position.y)
@@ -11,5 +15,15 @@ public class CameraChase : MonoBehaviour
             Vector3 newPosition = new Vector3(transform.position.x, _mainGuy.position.y, transform.position.z);
             transform.position = newPosition;
         }
+    }
+    private void FixedUpdate()
+    {
+        Transform transform = GetComponent<Transform>();
+
+        // _currentSpeed = Mathf.Min(_currentSpeed + 0.001f, maxSpeed);
+
+        transform.position = new Vector3(transform.position.x,
+            transform.position.y + _currentSpeed,
+            transform.position.z);
     }
 }
--- Assets/PlatformSpawner.cs
+++ Assets/PlatformSpawner.cs
@@ -4,37 +4,50 @@ public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField] private Transform _mainGuy;
     [SerializeField] private GameObject _platformPrefab;
-    private int PLATFORM_CREATION_AMOUNT = 30;
+    private Vector3 lastBlockInPreviousBulkPosition;
+    private float _cameraLeftEdge;
+    private float _cameraRightEdge;
+    private int PLATFORM_CREATION_AMOUNT = 10;
     private double PLATFORM_HEIGHT = 0.3;
 
     private void Start()
     {
-        Vector3 cameraBottomPosition = new Vector3(transform.position.x,
-            Camera.main.transform.position.y - Camera.main.orthographicSize, transform.position.z);
-        CreatePlatformBulk(cameraBottomPosition);
+        Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        _cameraLeftEdge = screenBounds.x * (-1);
+        _cameraRightEdge = screenBounds.x;
+        lastBlockInPreviousBulkPosition = CreatePlatformBulk(Camera.main.transform.position);
     }
 
     private void Update()
     {
-
+        if (lastBlockInPreviousBulkPosition.y - _mainGuy.transform.position.y <= 20)
+        {
+            lastBlockInPreviousBulkPosition = CreatePlatformBulk(lastBlockInPreviousBulkPosition);
+        }
     }
 
-    private void CreatePlatformBulk(Vector3 lastBlockPosition)
+    private Vector3 CreatePlatformBulk(Vector3 lastBlockInPreviousBulkPosition)
     {
-        float cameraLeftEdge = Camera.main.transform.position.x - Camera.main.transform.localScale.x;
-        float cameraRightEdge = Camera.main.transform.position.x + Camera.main.transform.localScale.x;
-
+        Vector3 previousBlockPosition = new Vector3();
         for (int i = 0; i < PLATFORM_CREATION_AMOUNT; i++)
         {
-            float platfromSize = Random.Range(1, 4);
-            float platfromPositionX = Random.Range(cameraLeftEdge, cameraRightEdge);
-            float platformPositionY = lastBlockPosition.y + Random.Range(1, 2);
+            float platfromSize = Random.Range(4f, 8f);
+            float platfromPositionX = Random.Range(_cameraLeftEdge, _cameraRightEdge);
+            float platformPositionY = GetNewYPosition(i == 0 ? lastBlockInPreviousBulkPosition.y : previousBlockPosition.y);
 
             Vector3 platformPosition = new Vector3(platfromPositionX, platformPositionY, transform.position.z);
 
             GameObject platform = Instantiate(_platformPrefab, platformPosition, Quaternion.identity);
             platform.transform.localScale = new Vector3(platfromSize, (float)PLATFORM_HEIGHT, transform.localScale.z);
 
+            previousBlockPosition = platform.transform.localPosition;
         }
+
+        return previousBlockPosition;
+    }
+
+    private float GetNewYPosition(float previousY)
+    {
+        return previousY + Random.Range(2f, 3.5f);
     }
 }
